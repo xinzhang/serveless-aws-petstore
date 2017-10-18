91eac5367ce4b9a1dce278b2a7e095c915098a28
Xin Zhang <xin.zhang@essentialenergy.com.au>
2017-10-18 20:16:57 +1100

    init

---
+++ pets/create.js
@@ -0,0 +1,43 @@
+const uuid = require('uuid');
+
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.dynamoDb.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+    const timestamp = new Date().getTime();
+    const data = JSON.parse(event.body);
+
+    if (typeof data.petName !== 'string' || typeof data.petBreed !== 'string' ) {
+        console.error('Validation Failed');
+        callback(new Error('could not create the pet item'));
+        return;
+    }
+
+    const params = {
+        TableName : process.env.DYNAMODB_TABLE,
+        Item : {
+            id: uuid.v1(),
+            petName: data.petName,
+            petBreed : data.petBreed,
+            createdAt : timestamp,
+            updatedAt : timestamp,
+        }
+    }
+
+    dynamoDb.put(params, (error) => {
+        if (error) {
+            console.error(error);
+            callback(new Error('could not create the pet item'));
+            return;
+        }
+
+        const response = {
+            statusCode : 200, 
+            body: JSON.stringify(params.Item)
+        };
+
+        callback(null, response);
+    })
+
+}
